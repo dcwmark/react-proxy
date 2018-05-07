9f1b5d5fd5007eeac9e43e3285fafdc4a76530dc
Entaro Adun <dcwmark@gmail.com>
2018-05-06 22:12:11 -0400

    First commits

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send({ express: `response from 5000`});
+});
+
 app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
